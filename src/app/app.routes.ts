bb0eb3aa7c55e003025f756197d8668ef992448b
Michele Bufano <michele.bufano@ad.uniklinik-freiburg.de>
2026-05-26 11:44:14 +0200

    goes on

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+
+  // future viewer route
+//   {
+//     path: 'viewer',
+//     loadComponent: () =>
+//       import('./viewer/viewer.component').then(m => m.ViewerComponent)
+//   },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
